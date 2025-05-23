dd111b76d1a5aeffc526f2488c0bdfe26062b458
jiwans-oza <jiwans.oza.devfrend@gmail.com>
2025-05-23 08:58:46 +0530

    Delete app directory

--- app/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <div>
-      {/* Your profile page content here */}
-      <h1>Profile Page</h1>
-      <p>This is the profile page.</p>
-    </div>
-  )
-}
