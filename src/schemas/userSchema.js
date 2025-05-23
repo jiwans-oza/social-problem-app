0b5b29beef4710a2ff5ffb77060bb628a0355b94
jiwans-oza <jiwans.oza.devfrend@gmail.com>
2025-05-23 09:00:26 +0530

    added src

---
+++ src/schemas/userSchema.js
@@ -0,0 +1,15 @@
+export const userSchema = {
+  name: {
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  email: {
+    required: true,
+    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+  },
+  password: {
+    required: true,
+    minLength: 8,
+  },
+}; 
